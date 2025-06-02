99453b3a744ff32e68cde1c72760ef5b33002245
SimonWandera <simon.wandera@systechafrica.com>
2025-06-02 12:25:32 +0300

    Product entity, Inroduction to relationships

--- src/main/java/com/parworld/shop/model/Product.java
+++ src/main/java/com/parworld/shop/model/Product.java
@@ -1,9 +1,42 @@
 package com.parworld.shop.model;
 
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@AllArgsConstructor
-
+@Getter
+@Setter
+@Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private double price;
+
+    @Column
+    private int quantity;
+
+//    @Column
+//    private Long productTypeId;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "product_type_id")
+    private ProductType productType;
+
 }
