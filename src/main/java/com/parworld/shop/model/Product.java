bd2c95cfb029705ba883ff5c072c10ee9f62c312
SimonWandera <simon.wandera@systechafrica.com>
2025-06-02 12:30:24 +0300

    Intoduction to queries

--- src/main/java/com/parworld/shop/model/Product.java
+++ src/main/java/com/parworld/shop/model/Product.java
@@ -10,7 +10,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
--- src/main/java/com/parworld/shop/repository/ProductTypeRepository.java
+++ src/main/java/com/parworld/shop/repository/ProductTypeRepository.java
@@ -1,8 +1,14 @@
 package com.parworld.shop.repository;
 
-import com.parworld.shop.model.Product;
 import com.parworld.shop.model.ProductType;
+import com.parworld.shop.model.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductTypeRepository extends JpaRepository<ProductType, Long> {
+
+    @Query(nativeQuery = true, "select * from product_type")
+    ProductType getProductTypeByType(Type type);
+
+
 }
