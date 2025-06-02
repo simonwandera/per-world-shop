75c379bf8342f9da71ea9bba14ae44b6a167c3a3
SimonWandera <simon.wandera@systechafrica.com>
2025-06-02 21:21:06 +0300

    Native queries

--- src/main/java/com/parworld/shop/repository/ProductTypeRepository.java
+++ src/main/java/com/parworld/shop/repository/ProductTypeRepository.java
@@ -7,8 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface ProductTypeRepository extends JpaRepository<ProductType, Long> {
 
-    @Query(nativeQuery = true, "select * from product_type")
+    @Query(value = "select * from product_type where type=:type", nativeQuery = true)
     ProductType getProductTypeByType(Type type);
-
 
 }
